525283ac053a053dbf9a5828c331a8aeffef9a01
shanto <shantanushanto@gmail.com>
2020-03-02 03:28:44 -0600

    move file from backup to submodule

---
+++ JobLauncher.py
@@ -0,0 +1,224 @@
+import os
+import uuid
+
+from libpy import pyutils
+import router
+import time
+import dill
+
+class Task():
+    def __init__(self, cmd, out):
+        self.cmd = cmd
+        self.out = out
+
+    @staticmethod
+    def cache_tasks(tasks, dir):
+        # cache list of tasks.
+        task_cache_file = os.path.join(dir, f'.tasks_{time.time_ns()}_n-task-{len(tasks)}_{time.ctime().replace(" ", "-")}.tasks')
+        with open(task_cache_file, 'wb') as file:
+            dill.dump(tasks, file)
+
+    @staticmethod
+    def incomplete_tasks_from_cache(dir, file_name=None, finish_tag=pyutils.tag_job_finished_successfully):
+        # given dir get recent submitted tasks which are not completed
+        # file_name: if file_name is None use recent one
+        # complete_tag in err file to check if the task is completed
+
+        def get_tasks():
+            # get task from recent or given file
+            # get recent one
+            if file_name is None:
+                files = pyutils.files_with_extension(dir, '.tasks')
+                # get recent one
+                recent_file = sorted(files)[-1]
+            else:
+                recent_file = os.path.join(dir, file_name)
+
+            with open(recent_file, 'rb') as file:
+                tasks = dill.load(file)
+
+            return tasks
+
+        def get_incomplete_task(submitted_tasks):
+            # jobs that are incomplete. Either not file exist for that or success tag doesn't exist
+            incomplete_tasks = []
+            # check finish tag from each file or existence of file
+            for task in submitted_tasks:
+                err_path = os.path.join(dir, f'{task.out}.err')
+                if os.path.isfile(err_path): # check if file exist but finish tag doesn't exist
+                    with open(err_path, 'r') as f:
+                        # check finish tag
+                        if finish_tag not in f.read():
+                            incomplete_tasks.append(task)
+                else: # err file doesn't exist. Need to rerun
+                    incomplete_tasks.append(task)
+
+            return incomplete_tasks
+
+        # previously submitted task
+        submitted_tasks = get_tasks()
+        # find incomplete task
+        incomplete = get_incomplete_task(submitted_tasks)
+        return incomplete
+
+class JobLauncher():
+    def __init__(self, task_gen, tasks_each_launch, no_cpu_per_task, time, mem, sbatch_extra_cmd = ''):
+        '''
+        task_gen: is a function that returns list of Task objects. Task object has two parts.
+            cmd (string): command that need to be executed. Generally it is the executable with full path. ex. /home/shanto/exe_search
+            out (string): where the output will go after executing cmd. It is also with full path. 
+                          - Make sure out dir exists beforehand. 
+                          - Don't need to use [.out] at the end. [.out] is added with the specific job launcer. ex. /home/shanto/data/here_go_data
+        sbatch_extra_cmd: is the cmd that need to place in between sbatch header and executable. Normally it is used to load library or add path
+                          - make sure all the commands are with new line including last line 
+        tasks_each_launch: usually it is 1 for palab and n for terra cluster
+        '''
+        self.task_gen = task_gen
+        
+        self.no_tasks = tasks_each_launch 
+        self.no_cpu_per_task = no_cpu_per_task
+        self.time = time
+        self.mem = mem
+        
+        # temporary job directory to put jobs
+        self.job_dir = f'{os.getcwd()}/.job'
+        pyutils.mkdir_p(self.job_dir)
+
+        self.job_file_name = 'tmp.job' # file that submit task_file
+
+        self.sbatch_extra_cmd = sbatch_extra_cmd 
+    
+    def launch(self):
+        raise NotImplementedError
+
+class TamuLauncher(JobLauncher):
+    def __init__(self, task_gen, acc_id = 122818929441, tasks_each_launch = 42, no_cpu_per_task = 1, ntasks_per_node = 14, time = '00:40:00', mem = '50000M', job_name = 'job', sbatch_extra_cmd = ''):
+        '''
+        task_gen is a function that return list of Task
+        '''
+        super().__init__(task_gen, tasks_each_launch, no_cpu_per_task, time, mem, sbatch_extra_cmd)
+
+        self.job_name = job_name 
+        self.acc_id = acc_id 
+        self.ntasks_per_node = ntasks_per_node
+
+        self.task_file_name = 'tasks' # file that contain all commands to run the exe
+    
+    def _sbatch_header(self):
+        header = (
+            f'#!/bin/bash\n'
+            f'#SBATCH --export=NONE\n'               
+            f'#SBATCH --get-user-env=L\n'            
+            f'#SBATCH --job-name={self.job_name}\n'
+            f'#SBATCH --output={self.job_dir}/{self.job_name}.%j\n'
+            f'#SBATCH --time={self.time}\n'            
+            f'#SBATCH --ntasks={self.no_tasks}\n'       
+            f'#SBATCH --ntasks-per-node={self.ntasks_per_node}\n'
+            f'#SBATCH --cpus-per-task={self.no_cpu_per_task}\n'
+            f'#SBATCH --mem={self.mem}\n'
+            f'#SBATCH --account={self.acc_id}\n'
+            )
+        return header 
+
+
+    def _sbatch_script(self, file):
+        script = (
+            f'{self._sbatch_header()}\n'
+            f'source .TerraModuleLoad.sh\n'
+            f'{self.sbatch_extra_cmd}\n'
+            f'tamulauncher {file}\n'
+            )
+        return script 
+    
+    def _get_tasks_file(self, tasks):
+        
+        tasks_job = ''
+        for task in tasks:
+            tasks_job += f'{task.cmd} 2> {task.out}.err 1> {task.out}.out\n'
+        
+        unique_file_name = f'{self.task_file_name}_{uuid.uuid4()}'
+        tasks_file = os.path.join(self.job_dir, unique_file_name)
+        with open(tasks_file, 'w') as fh:
+            fh.writelines(tasks_job)
+        return tasks_file 
+    
+    def check_launcher_cache(self):
+        tamulauncher_cache = '.tamulauncher-log'
+        pyutils.dir_choice(tamulauncher_cache)
+
+    def launch(self):
+
+        tasks = self.task_gen()
+        self.check_launcher_cache()
+
+        for i in range(0, len(tasks), self.no_tasks):
+            tasks_job = self._get_tasks_file(tasks[i:i+self.no_tasks])
+            script = self._sbatch_script(tasks_job)
+            
+            u_job_file_name = f'{self.job_file_name}_{uuid.uuid4()}'
+            job_file = os.path.join(self.job_dir, u_job_file_name)
+            with open(job_file, 'w') as fh:
+                fh.writelines(script)
+            print(f'sbatch {job_file}')
+            os.system(f'sbatch {job_file}')
+
+
+class PAlabLauncher(JobLauncher):
+    def __init__(self, task_gen, tasks_each_launch = 1, no_cpu_per_task = 1, time = '00:40:00', mem = '50000M', sbatch_extra_cmd = '', no_exclude_node = 1):
+        '''
+        Make sure all the directories are already exist
+        task_gen is a function that return list of Task
+        no_exclude_node: how many node not to use
+        '''
+        super().__init__(task_gen, tasks_each_launch, no_cpu_per_task, time, mem,  sbatch_extra_cmd)
+        self.no_exclude_node = no_exclude_node 
+
+    def _sbatch_header(self, job_name, out):
+        header = (
+            f'#!/bin/bash\n'
+            f'#SBATCH --job-name={job_name}\n'
+            f'#SBATCH --output={out}.out\n'
+            f'#SBATCH --error={out}.err\n'
+            f'#SBATCH --cpus-per-task={self.no_cpu_per_task}\n'
+            )
+        
+        # excluding node
+        if self.no_exclude_node > 0:
+            exclude = f'01-0{self.no_exclude_node}'
+            header += (
+                    f'#SBATCH --exclude=node[{exclude}]\n'
+                    )
+
+        return header 
+    
+    def _sbatch_script(self, header, cmd):
+        script = (
+            f'{header}\n'
+            f'{self.sbatch_extra_cmd}\n'
+            f'{cmd}\n'
+            )
+        return script 
+    
+    def launch(self):
+
+        tasks = self.task_gen()
+
+        for task in tasks:
+            job_name = os.path.basename(task.out) # take the output file name as job name as output file name is unique
+            header = self._sbatch_header(job_name, task.out)
+            cmd = task.cmd
+
+            job_script = self._sbatch_script(header, cmd)
+
+            out_file_name = os.path.basename(task.out)
+            u_job_file_name = f'{self.job_file_name}_{out_file_name}_{uuid.uuid4()}'
+            job_file = os.path.join(self.job_dir, u_job_file_name)
+            with open(job_file, 'w') as fh:
+                fh.write(job_script)
+
+            print(f'sbatch {job_file}')
+            os.system(f'sbatch {job_file}') 
+            
+
+
+
---
+++ pyutils.py
@@ -0,0 +1,65 @@
+import os 
+import shutil
+import time
+import json
+import sys
+
+def mkdir_p(dir, verbose = False):
+    '''make a directory (dir) if it doesn't exist'''
+    if not os.path.exists(dir):
+        if verbose is True: print(f'-> Created new dir named: {dir}')
+        os.mkdir(dir)
+
+def dir_choice(dir, verbose = True):
+    if os.path.exists(dir):
+        
+        # number of file in the directory
+        tot_file = len(os.listdir(dir))
+        
+        # take input to execute
+        inp = input(f'{dir} exist! Total file inside: {tot_file}\nNew: n, Delete: d, Empty directory: e, Continue: c, Abort a -> ')
+        if inp == 'e':
+            if verbose is True: print(f'-> Cleaning inside of the dir: {dir}')
+            shutil.rmtree(dir) # delete all files and folder inside it
+            mkdir_p(dir)
+        if inp == 'd':
+            if verbose is True: print(f'-> Deleting the dir: {dir}')
+            shutil.rmtree(dir)
+            exit(0)
+        if inp == 'a':
+            if verbose is True: print('-> Aborting...')
+            exit(0)
+        if inp == 'n':
+            # take new directory name
+            new_dir = input(f'Enter your desired directory name: ')
+            new_dir = os.path.join(os.path.dirname(dir), new_dir)
+            # if new dir name exist do same process again
+            return dir_choice(new_dir, verbose)
+    else:
+        mkdir_p(dir, verbose)
+
+    return dir
+
+def files_with_extension(dir, extension):
+    # given directory and extension get all files. All these files has full path
+    files = []
+    for file in os.listdir(dir):
+        if file.endswith(extension):
+            files.append(os.path.join(dir, file))
+    return files
+
+def merge_dict(d,d1):
+    for k,v in d1.items():
+        if (k in d):
+            d[k].update(d1[k])
+        else:
+            d[k] = d1[k]
+    return d
+
+tag_job_finished_successfully = 'JobFinishedSuccessfully'
+def print_log(log):
+    # print final log dictionary with termination message
+    js = json.dumps(dict(log))
+    print(js)
+    # tag use to identify successfully finished job
+    sys.stderr.write(f'{time.ctime()}\n{tag_job_finished_successfully}\n')
