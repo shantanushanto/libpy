8f50d6fdbbd98711e2347844212cfa60b7837482
shanto <shantanushanto@gmail.com>
2020-03-18 18:05:15 -0500

    add readme method

---
+++ commonutils.py
@@ -0,0 +1,8 @@
+import os
+import time
+
+# append readme text given a directory
+def readme(dir):
+    desc = input('Give small description of the experiment.\n')
+    with open(os.path.join(dir, 'readme'), 'a') as f:
+        f.write(f'Date created: {time.ctime()} -> {desc}\n\n')
