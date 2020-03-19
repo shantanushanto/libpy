bb0dc10e372052a32be921c6403ec34ad39b1a57
shanto <shantanushanto@gmail.com>
2020-03-18 23:03:33 -0500

    update input format for readme

--- commonutils.py
+++ commonutils.py
@@ -3,6 +3,6 @@ import time
 
 # append readme text given a directory
 def readme(dir):
-    desc = input('Give small description of the experiment.\n')
+    desc = input('Give small description of the experiment. -> ')
     with open(os.path.join(dir, 'readme'), 'a') as f:
         f.write(f'Date created: {time.ctime()} -> {desc}\n\n')
