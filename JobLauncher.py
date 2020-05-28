eced9829b1de74a075b32c667f3e75bc4c4dc5de
shanto <shantanushanto@gmail.com>
2020-05-27 21:00:19 -0500

    update module name for terra keras gpu

--- JobLauncher.py
+++ JobLauncher.py
@@ -363,7 +363,7 @@ def launch_job(cluster, callback_batch_gen, job_name, no_cpu=1, time='3:00:00', 
         server = AtlasLauncher(callback_batch_gen, sbatch_extra_cmd=sbatch_extra_cmd, no_cpu_per_task=no_cpu, atlas_ratio=atlas_ratio, submission_check=submission_check)
     elif cluster == 'tamulauncher':
         import router  # as router may not be present in every project importing here
-        sbatch_extra_cmd = f'source {os.path.join(router.project_root, "TerraModule.sh")}'
+        sbatch_extra_cmd = f'source {os.path.join(router.project_root, "TerraModuleCPU.sh")}'
         server = TamuLauncher(callback_batch_gen, job_name=job_name, acc_id=acc_id, sbatch_extra_cmd=sbatch_extra_cmd, time=time, submission_check=submission_check)
     elif cluster == 'terragpu':
         import router  # as router may not be present in every project importing here
